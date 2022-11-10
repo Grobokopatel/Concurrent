ea36a9a7eff954a8c905d458a9eea2f5475df301
Egor_Usov <egotronn@gmail.com>
2022-11-10 23:48:15 +0500

    Optimized timer

--- TimeQuantum/Program.cs
+++ TimeQuantum/Program.cs
@@ -12,28 +12,31 @@ namespace TimeQuantum
             Process.GetCurrentProcess().ProcessorAffinity = (IntPtr)(1 << (Environment.ProcessorCount - 1));
 
             CancellationToken = true;
-            var stopWatch = new Stopwatch();
-            stopWatch.Start();
+            
 
             var firstThread = new Thread(() => { while (CancellationToken) { } })
             {
                 IsBackground = true,
                 Priority = ThreadPriority.Normal,
             };
-            firstThread.Start();
+            
 
             var secondThread = new Thread(() => { CancellationToken = false; })
             {
                 IsBackground = true,
                 Priority = ThreadPriority.Normal,
             };
-            secondThread.Start();
+            var stopWatch = new Stopwatch();
 
-            firstThread.Join();
+            firstThread.Start();
+            secondThread.Start();
+            stopWatch.Start();
+
             secondThread.Join();
             stopWatch.Stop();
 
             Console.WriteLine(stopWatch.ElapsedMilliseconds);
+            firstThread.Join();
         }
     }
 }
