77d8608896c6bb9397403f5152462ce2d7228686
Egor_Usov <egotronn@gmail.com>
2022-12-14 03:35:59 +0500

    Did fourth homework

---
+++ 4. CustomThreadPool/ThreadPools/MyThreadPool.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CustomThreadPool.ThreadPools
+{
+    internal class MyThreadPool : IThreadPool
+    {
+        internal readonly ConcurrentQueue<Action> globalQueue;
+        internal readonly Worker[] workers;
+        internal volatile int waitingThreadsCount;
+        internal long tasksProcessedCount;
+
+        public MyThreadPool(int workerCount)
+        {
+            globalQueue = new ConcurrentQueue<Action>();
+            workers = new Worker[workerCount];
+            for (var i = 0; i < workerCount; ++i)
+                workers[i] = new Worker(this);
+            for (var i = 0; i < workerCount; ++i)
+                workers[i].Start();
+        }
+
+        public MyThreadPool() : this(Environment.ProcessorCount) { }
+
+        public void EnqueueAction(Action action)
+        {
+            globalQueue.Enqueue(action);
+
+            if (waitingThreadsCount > 0)
+            {
+                lock (globalQueue)
+                {
+                    Monitor.Pulse(globalQueue);
+                }
+            }
+        }
+
+        public long GetTasksProcessedCount() => tasksProcessedCount;
+    }
+}
---
+++ 4. CustomThreadPool/ThreadPools/Worker.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CustomThreadPool.ThreadPools
+{
+    internal class Worker
+    {
+        private readonly WorkStealingQueue<Action> _localQueue;
+        private readonly Thread _thread;
+        private readonly MyThreadPool _parentThreadPool;
+
+        public Worker(MyThreadPool parentThreadPool)
+        {
+            _localQueue = new WorkStealingQueue<Action>();
+            _parentThreadPool = parentThreadPool;
+            _thread = new Thread(WorkLoop) { IsBackground = true };
+        }
+
+        public void Start()
+        {
+            _thread.Start();
+        }
+
+        private void WorkLoop()
+        {
+            while (true)
+            {
+                GetTask()();
+                Interlocked.Increment(ref _parentThreadPool.tasksProcessedCount);
+            }
+        }
+
+        private Action GetTask()
+        {
+            if (TryGetTaskFromLocalQueue(out var task))
+                return task;
+
+            while (true)
+            {
+                if (TryGetTaskFromGlobalQueue(out task))
+                    return task;
+
+                if (TryStealTask(out task))
+                    return task;
+
+                WaitForNewTask();
+            }
+        }
+
+        private bool TryGetTaskFromLocalQueue(out Action task)
+        {
+            task = null;
+            return _localQueue.LocalPop(ref task);
+        }
+
+        private bool TryGetTaskFromGlobalQueue(out Action task)
+        {
+            return _parentThreadPool.globalQueue.TryDequeue(out task);
+        }
+
+        private bool TryStealTask(out Action task)
+        {
+            task = null;
+            foreach (var otherWorker in _parentThreadPool.workers)
+            {
+                if (otherWorker != this)
+                {
+                    if (otherWorker._localQueue.TrySteal(ref task))
+                        return true;
+                }
+            }
+
+            return false;
+        }
+
+        internal void WaitForNewTask()
+        {
+            var globalQueue = _parentThreadPool.globalQueue;
+            ref int waitingThreadCount = ref _parentThreadPool.waitingThreadsCount;
+            lock (globalQueue)
+            {
+                ++waitingThreadCount;
+                try
+                {
+                    Monitor.Wait(globalQueue);
+                }
+                finally
+                {
+                    --waitingThreadCount;
+                }
+            }
+        }
+    }
+}
